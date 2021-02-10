3faf162c08f2012ab702b95e4c14dfd489e0d9a7
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-02-10 16:41:42 +0100

    Make the example work with current demo.

--- shellscript/verify.sh
+++ shellscript/verify.sh
@@ -6,17 +6,29 @@ if [ $# -gt 1 ]; then
 	echo "Syntax: $0 [client.crt]"
 	exit 1
 fi
+CERT=${1:-client.crt}
 
-CERT=${2:-client.crt}
-JSON=$(cat)
+JSON=$(cat | tr -d '\\' | tr -d '\n\r' )
+
 TMPDIR=${TMPDIR:-/tmp}
+CA=${CA:-$CERT}
+PURPOSE=${PURPOSE:-any}
 
-printf "$JSON" | tr -d '\n\r' | sed -e 's/.*payload":.?*"//' -e 's/".*//' > "$TMPDIR/payload.$$.bin"
+printf "$JSON" | sed -e 's/.*payload":.?*"//' -e 's/".*//'  > "$TMPDIR/payload.$$.bin"
 
+# Allow for partial chain and any purpose for
+# testing and debugging purposes.
+#
 printf "$JSON" | \
-	tr -d '\n\r' | \
 	sed -e 's/.*signature":.?*"//' -e 's/".*//' |\
 	base64 -d |\
-	openssl cms -verify -CAfile client.crt -content "$TMPDIR/payload.$$.bin" -inform DER -binary | base64 -d
+	openssl cms -verify \
+		-content "$TMPDIR/payload.$$.bin" -inform DER -binary \
+		-CAfile "$CA" \
+		-certfile client.crt \
+		-partial_chain \
+		-purpose $PURPOSE \
+	| \
+	base64 -d
 
 rm -f "$TMPDIR/payload.$$.bin"
