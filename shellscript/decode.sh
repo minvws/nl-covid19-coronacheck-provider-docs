d468c7fae752eccaee5993ab21d82712a402da8f
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-02-11 14:03:30 +0100

    Accept other types of json too.

--- shellscript/decode.sh
+++ shellscript/decode.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-grep signature | sed -e 's/.*"M/M/' -e 's/".*//' |  base64 -d | openssl pkcs7 -inform DER -noout -print
+#
+if [ $# -ne 0 ]; then
+	echo Syntax: $0
+	exit 1
+fi
+
+grep signature |\
+	tr -d '\\' |\
+	tr -d '\n\r' |\
+	sed -e 's/.*"M/M/' -e 's/".*//' |  \
+	base64 -d |\
+	openssl pkcs7 -inform DER -noout -print
