78f3d0a301d876af1ebfb181256cc732d6be9130
Dirk-Willem van Gulik <dirkx@webweaving.org>
2021-02-11 13:40:36 +0100

    Add better faked chain

--- shellscript/sign.sh
+++ shellscript/sign.sh
@@ -11,27 +11,7 @@ JSON=${1:-example.json}
 CERT=${2:-client.crt}
 
 if [ $# -lt 2 -a ! -e client.crt ]; then
-	echo Generating a self signed demo certificate.
-	echo
-
-	ERM=$(
-              SERIAL=$(openssl rand -hex 1024 | tr -d a-zA-Z | cut -c 1-16)
-              openssl req \
-		-new -x509 \
-		-extensions v3_req \
-		-addext subjectKeyIdentifier=hash \
-		-addext authorityKeyIdentifier=keyid,issuer \
-		-subj "/CN=Locatie Noord/O=Testers-are-us/C=NL" \
-		-keyout client.crt -out client.crt -nodes -set_serial $SERIAL 2>&1
-
-	     openssl pkcs12 -in client.crt -export -out keystore.p12 \
-		-passout pass:changeme -name "1" 2>&1
-	) 
-	if [ $? -ne 0 ]; then
-		echo cert generation failed: $ERM
-		echo Aborted.
-		exit $?
-	fi
+	. ./gen-fake-pki-overheid.sh
 fi
 
 JSON_B64=$(base64 "$JSON")
@@ -39,7 +19,7 @@ JSON_B64=$(base64 "$JSON")
 # We avoid using echo (shell and /bin echo behave differently) as to 
 # get control over the trialing carriage return.
 
-SIG_B64=$(printf "$JSON_B64" | openssl cms -sign -outform DER -signer "$CERT" | base64)
+SIG_B64=$(printf "$JSON_B64" | openssl cms -sign -outform DER -signer "$CERT" -certfile chain.pem| base64)
 
 cat <<EOM
 [
