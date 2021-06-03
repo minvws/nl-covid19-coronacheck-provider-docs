b3c1015ad2fd63b18106cc210b76ffd7b3c27e1a
Ivo Jansch <ivo@egeniq.com>
2021-06-03 17:30:51 +0200

    Fix 'header too long' on certain payloads by usig -r instead of manual quote magic

--- shellscript/verify.sh
+++ shellscript/verify.sh
@@ -12,7 +12,6 @@ CA=${1:-ca-pki-overheid.pem}
 PURPOSE=${PURPOSE:-any}
 OPENSSL=${OPENSSL:-openssl}
 
-
 if $OPENSSL version | grep -q LibreSSL; then
         echo Sorry - OpenSSL is needed.
         exit 1
@@ -35,10 +34,10 @@ fi
 # Allow for partial chain and any purpose for
 # testing and debugging purposes. 
 #
-printf "$JSON" | jq .signature | sed -e 's/^"//' -e 's/"$//' |
+printf "$JSON" | jq -r .signature | 
 	base64 -d  > x.raw
 
-printf "$JSON" | jq .signature | sed -e 's/^"//' -e 's/"$//' |
+printf "$JSON" | jq -r .signature |
 	base64 -d |\
 	$OPENSSL cms -verify \
 		-content "$TMPDIR/payload.$$.bin" -inform DER -binary \
